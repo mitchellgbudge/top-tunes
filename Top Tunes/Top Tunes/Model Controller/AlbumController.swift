cad025053dbab78f01b89ebd9baa39e803ffc84d
Mitchell Budge <mitchellbudge@gmail.com>
2020-03-03 15:31:20 -0700

    Stage album controller class

---
+++ Top Tunes/Top Tunes/Model Controller/AlbumController.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumController.swift
+//  Top Tunes
+//
+//  Created by Mitchell Budge on 3/3/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
+
+class AlbumController {
+    
+    
+    
+}
