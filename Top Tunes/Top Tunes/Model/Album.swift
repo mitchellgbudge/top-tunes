7cb8bc54f20e54d7fa0e0d0ee0173fda1a9b09d6
Mitchell Budge <mitchellbudge@gmail.com>
2020-03-03 15:26:59 -0700

    Scaffold model object with album name, artist name, and artwork url

--- Top Tunes/Top Tunes/Model/Album.swift
+++ Top Tunes/Top Tunes/Model/Album.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct Album {
+    let albumName: String
+    let artistName: String
+    let artworkURL: String
+    
+    enum CodingKeys: String, CodingKey {
+        case feed
+        case results
+        case albumName
+        case artistName
+        case artworkURL
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let feedContainer = try container.nestedContainer(keyedBy: CodingKeys.self, forKey: .feed)
+        var resultsContainer = try feedContainer.nestedUnkeyedContainer(forKey: .results)
+        let albumContainer = try resultsContainer.nestedContainer(keyedBy: CodingKeys.self)
+        albumName = try albumContainer.decode(String.self, forKey: .albumName)
+        artistName = try albumContainer.decode(String.self, forKey: .artistName)
+        artworkURL = try albumContainer.decode(String.self, forKey: .artworkURL)
+    }
+    
+}
