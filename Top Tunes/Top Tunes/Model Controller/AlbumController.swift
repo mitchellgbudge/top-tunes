92611d6378071f0a8d563efb90f144682927f736
Mitchell Budge <mitchellbudge@gmail.com>
2020-03-03 15:38:53 -0700

    Configure controller class with album fetch method

--- Top Tunes/Top Tunes/Model Controller/AlbumController.swift
+++ Top Tunes/Top Tunes/Model Controller/AlbumController.swift
@@ -10,6 +10,35 @@ import Foundation
 
 class AlbumController {
     
+    var albums: [Album] = []
     
+    let baseURL = URL(string: "https://rss.itunes.apple.com/api/v1/us/apple-music/top-albums/all/100/explicit.json/")
+    
+    func fetchAlbums(completion: @escaping (Result<Album, Error>) -> Void) {
+        
+        guard let requestURL = baseURL else { return }
+        
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                print("Error fetching albums: \(error)")
+                completion(.failure(error))
+                return
+            }
+            guard let albumData = data else {
+                print("Error retrieving data from data task")
+                completion(.failure(NSError()))
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                self.albums = try decoder.decode([Album].self, from: albumData)
+                print(self.albums.count)
+            } catch {
+                print("Error decoding data to type Album: \(error)")
+                completion(.failure(error))
+            }
+        }.resume()
+    }
     
 }
