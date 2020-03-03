823d990a0c8b34d1566f6f9a2976b451e656638e
Mitchell Budge <mitchellbudge@gmail.com>
2020-03-03 15:02:47 -0700

    Create initial Xcode project

---
+++ Top Tunes/Top Tunes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Tunes
+//
+//  Created by Mitchell Budge on 3/3/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top Tunes/Top TunesTests/Top_TunesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_TunesTests.swift
+//  Top TunesTests
+//
+//  Created by Mitchell Budge on 3/3/20.
+//  Copyright © 2020 Mitchell Budge. All rights reserved.
+//
+
+import XCTest
+@testable import Top_Tunes
+
+class Top_TunesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
